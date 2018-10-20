37935a46b00fff94991e97df431af37093c88e12
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-10-20 12:36:06 -0300

    Carga incial do exemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appanimationcss';
+export class AppComponent  implements OnInit {
+  
+  @ViewChild('cover') cover: ElementRef;
+  WAITLOADPAGE: string = "waitLoadPage";
+
+  constructor() { }
+
+  ngOnInit() {
+    this.cover.nativeElement.classList.add(this.WAITLOADPAGE);
+  }
+
+  onLoad() {
+    this.cover.nativeElement.classList.remove(this.WAITLOADPAGE);
+  }
+  
 }
